14096600a07b00171c6d12a5eac5bb08dc16d01e
binux <17175297.hk@gmail.com>
2011-03-06 22:36:11 +0800

    chinese_digit.py encoding support

--- python/chinese_digit.py
+++ python/chinese_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
+# author: binux(17175297.hk@gmail.com)
 
 dict ={u'零':0, u'一':1, u'二':2, u'三':3, u'四':4, u'五':5, u'六':6, u'七':7, u'八':8, u'九':9, u'十':10, u'百':100, u'千':1000, u'万':10000,
        u'０':0, u'１':1, u'２':2, u'３':3, u'４':4, u'５':5, u'６':6, u'７':7, u'８':8, u'９':9,
                 u'壹':1, u'贰':2, u'叁':3, u'肆':4, u'伍':5, u'陆':6, u'柒':7, u'捌':8, u'玖':9, u'拾':10, u'佰':100, u'仟':1000, u'萬':10000,
        u'亿':100000000}
-def getResultForDigit(a):
+def getResultForDigit(a, encoding="utf-8"):
     if isinstance(a, str):
-        a = a.decode("utf-8")
+        a = a.decode(encoding)
 
     count = 0 
     result = 0
